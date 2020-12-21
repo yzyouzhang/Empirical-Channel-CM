81817bbe1abd1aee84d4d1fa0ef8610598a592d0
yzyouzhang <yzyouzhang@gmail.com>
2020-12-21 16:23:37 -0500

    preprocess librispeech LFCC

--- dataset.py
+++ dataset.py
@@ -3,7 +3,6 @@
 import numpy as np
 import torch
 from torch import Tensor
-import librosa
 from torch.utils.data import Dataset, DataLoader
 import scipy.io as sio
 import pickle
@@ -15,12 +14,8 @@ from typing import Any, Tuple, Union
 from pathlib import Path
 from utils import download_url, extract_archive, walk_files
 
+
 torch.set_default_tensor_type(torch.FloatTensor)
-URL = "https://datashare.is.ed.ac.uk/bitstream/handle/10283/3443/VCTK-Corpus-0.92.zip"
-FOLDER_IN_ARCHIVE = "VCTK-Corpus"
-_CHECKSUMS = {
-    "https://datashare.is.ed.ac.uk/bitstream/handle/10283/3443/VCTK-Corpus-0.92.zip": "8a6ba2946b36fcbef0212cad601f4bfa"
-}
 
 SampleType = Tuple[Tensor, int, str, str, str]
 
@@ -28,6 +23,87 @@ def torchaudio_load(filepath):
     wave, sr = librosa.load(filepath, sr=16000)
     waveform = torch.Tensor(np.expand_dims(wave, axis=0))
     return [waveform, sr]
+
+def load_librispeech_item(fileid: str,
+                          path: str,
+                          ext_audio: str,
+                          ext_txt: str) -> Tuple[Tensor, int, str, int, int, int]:
+    speaker_id, chapter_id, utterance_id = fileid.split("-")
+
+    file_text = speaker_id + "-" + chapter_id + ext_txt
+    file_text = os.path.join(path, speaker_id, chapter_id, file_text)
+
+    fileid_audio = speaker_id + "-" + chapter_id + "-" + utterance_id
+    file_audio = fileid_audio + ext_audio
+    file_audio = os.path.join(path, speaker_id, chapter_id, file_audio)
+
+    # Load audio
+    waveform, sample_rate = torchaudio_load(file_audio)
+
+    # Load text
+    with open(file_text) as ft:
+        for line in ft:
+            fileid_text, utterance = line.strip().split(" ", 1)
+            if fileid_audio == fileid_text:
+                break
+        else:
+            # Translation not found
+            raise FileNotFoundError("Translation not found for " + fileid_audio)
+
+    return (
+        waveform,
+        sample_rate,
+        utterance,
+        int(speaker_id),
+        int(chapter_id),
+        int(utterance_id),
+    )
+
+class LIBRISPEECH(Dataset):
+    """Create a Dataset for LibriSpeech.
+    Args:
+        root (str or Path): Path to the directory where the dataset is found or downloaded.
+        url (str, optional): The URL to download the dataset from,
+            or the type of the dataset to dowload.
+            Allowed type values are ``"dev-clean"``, ``"dev-other"``, ``"test-clean"``,
+            ``"test-other"``, ``"train-clean-100"``, ``"train-clean-360"`` and
+            ``"train-other-500"``. (default: ``"train-clean-100"``)
+        folder_in_archive (str, optional):
+            The top-level directory of the dataset. (default: ``"LibriSpeech"``)
+        download (bool, optional):
+            Whether to download the dataset if it is not found at root path. (default: ``False``).
+    """
+
+    _ext_txt = ".trans.txt"
+    _ext_audio = ".flac"
+
+    def __init__(self,
+                 root: Union[str, Path],
+                 basename: str = "train-clean-360",
+                 folder_in_archive: str = "LibriSpeech") -> None:
+
+        # Get string representation of 'root' in case Path object is passed
+        root = os.fspath(root)
+
+        folder_in_archive = os.path.join(folder_in_archive, basename)
+
+        self._path = os.path.join(root, folder_in_archive)
+
+        self._walker = sorted(str(p.stem) for p in Path(self._path).glob('*/*/*' + self._ext_audio))
+
+    def __getitem__(self, n: int) -> Tuple[Tensor, int, str, int, int, int]:
+        """Load the n-th sample from the dataset.
+        Args:
+            n (int): The index of the sample to be loaded
+        Returns:
+            tuple: ``(waveform, sample_rate, utterance, speaker_id, chapter_id, utterance_id)``
+        """
+        fileid = self._walker[n]
+        return load_librispeech_item(fileid, self._path, self._ext_audio, self._ext_txt)
+
+    def __len__(self) -> int:
+        return len(self._walker)
+
 
 class VCTK_092(Dataset):
     """Create VCTK 0.92 Dataset
@@ -51,7 +127,7 @@ class VCTK_092(Dataset):
         root: str,
         mic_id: str = "mic2",
         download: bool = False,
-        url: str = URL,
+        url: str = "https://datashare.is.ed.ac.uk/bitstream/handle/10283/3443/VCTK-Corpus-0.92.zip",
         audio_ext=".flac",
     ):
         if mic_id not in ["mic1", "mic2"]:
@@ -70,7 +146,7 @@ class VCTK_092(Dataset):
         if download:
             if not os.path.isdir(self._path):
                 if not os.path.isfile(archive):
-                    checksum = _CHECKSUMS.get(url, None)
+                    checksum = {"https://datashare.is.ed.ac.uk/bitstream/handle/10283/3443/VCTK-Corpus-0.92.zip": "8a6ba2946b36fcbef0212cad601f4bfa"}.get(url, None)
                     download_url(url, root, hash_value=checksum, hash_type="md5")
                 extract_archive(archive, self._path)
 
@@ -151,6 +227,7 @@ class VCTK_092(Dataset):
 class ASVspoof2019(Dataset):
     def __init__(self, access_type, path_to_database, path_to_features, path_to_protocol, part='train', feature='CQCC',
                  genuine_only=False, feat_len=650, pad_chop=True, padding='zero'):
+        super(ASVspoof2019, self).__init__()
         self.access_type = access_type
         self.ptd = path_to_database
         self.path_to_features = path_to_features
@@ -226,6 +303,41 @@ class ASVspoof2019(Dataset):
     def collate_fn(self, samples):
         return default_collate(samples)
 
+class LibriGenuine(Dataset):
+    def __init__(self, path_to_features, feature='LFCC', feat_len=750, pad_chop=True, padding='repeat'):
+        super(LibriGenuine, self).__init__()
+        self.ptf = path_to_features
+        self.feat_len = feat_len
+        self.feature = feature
+        self.pad_chop = pad_chop
+        self.padding = padding
+
+        self.all_files = librosa.util.find_files(os.path.join(self.ptf, self.feature), ext="pt")
+
+    def __len__(self):
+        return len(self.all_files)
+
+    def __getitem__(self, idx):
+        filepath = self.all_files[idx]
+        featureTensor = torch.load(filepath)
+        this_feat_len = featureTensor.shape[1]
+        if this_feat_len > self.feat_len:
+            startp = np.random.randint(this_feat_len-self.feat_len)
+            featureTensor = featureTensor[:, startp:startp+self.feat_len, :]
+        if this_feat_len < self.feat_len:
+            if self.padding == 'zero':
+                feat_mat = padding(featureTensor, self.feat_len)
+            elif self.padding == 'repeat':
+                feat_mat = repeat_padding(featureTensor, self.feat_len)
+            else:
+                raise ValueError('Padding should be zero or repeat!')
+
+        return featureTensor, 0, 0
+
+    def collate_fn(self, samples):
+        return default_collate(samples)
+
+
 def padding(spec, ref_len):
     width, cur_len = spec.shape
     assert ref_len > cur_len
@@ -240,18 +352,19 @@ def repeat_padding(spec, ref_len):
 
 
 if __name__ == "__main__":
-    path_to_database = '/data/neil/DS_10283_3336/'  # if run on GPU
-    path_to_features = '/dataNVME/neil/ASVspoof2019LAFeatures/'  # if run on GPU
-    path_to_protocol = '/data/neil/DS_10283_3336/LA/ASVspoof2019_LA_cm_protocols/'
-    training_set = ASVspoof2019("LA", path_to_database, path_to_features, path_to_protocol, genuine_only=False, pad_chop=False, feature='LFCC', feat_len=320)
-    feat_mat, audio_fn, tag, label = training_set[2999]
-    print(len(training_set))
-    print(audio_fn)
-    print(feat_mat.shape)
-    # print(cqcc.shape)
-    # print(lfcc.shape)
-    print(tag)
-    print(label)
+    # path_to_database = '/data/neil/DS_10283_3336/'  # if run on GPU
+    # path_to_features = '/dataNVME/neil/ASVspoof2019LAFeatures/'  # if run on GPU
+    # path_to_protocol = '/data/neil/DS_10283_3336/LA/ASVspoof2019_LA_cm_protocols/'
+    # training_set = ASVspoof2019("LA", path_to_database, path_to_features, path_to_protocol, genuine_only=False, pad_chop=False, feature='LFCC', feat_len=320)
+    # feat_mat, audio_fn, tag, label = training_set[2999]
+    # print(len(training_set))
+    # print(audio_fn)
+    # print(feat_mat.shape)
+    # # print(cqcc.shape)
+    # # print(lfcc.shape)
+    # print(tag)
+    # print(label)
+
     # samples = [training_set[26], training_set[27], training_set[28], training_set[29]]
     # out = training_set.collate_fn(samples)
 
@@ -265,17 +378,33 @@ if __name__ == "__main__":
     # print(tag)
     # print(label)
 
-    trainDataLoader = DataLoader(training_set, batch_size=32, shuffle=True, num_workers=0, collate_fn=training_set.collate_fn)
-    feat_mat_batch, audio_fn, tags, labels = [d for d in next(iter(trainDataLoader))]
-    print(feat_mat_batch.shape)
-    # print(feat_mat_batch)
+    # trainDataLoader = DataLoader(training_set, batch_size=32, shuffle=True, num_workers=0, collate_fn=training_set.collate_fn)
+    # feat_mat_batch, audio_fn, tags, labels = [d for d in next(iter(trainDataLoader))]
+    # print(feat_mat_batch.shape)
+    # # print(feat_mat_batch)
+    #
+    # vctk = VCTK_092(root="/data/neil/VCTK", download=False)
+    # print(len(vctk))
+    # waveform, sample_rate, utterance, speaker_id, utterance_id = vctk[124]
+    # print(waveform.shape)
+    # print(sample_rate)
+    # print(utterance)
+    # print(speaker_id)
+    # print(utterance_id)
 
-    vctk = VCTK_092(root="/data/neil/VCTK", download=False)
-    print(len(vctk))
-    waveform, sample_rate, utterance, speaker_id, utterance_id = vctk[124]
+    librispeech = LIBRISPEECH(root="/data/neil")
+    print(len(librispeech))
+    waveform, sample_rate, utterance, speaker_id, chapter_id, utterance_id = librispeech[164]
     print(waveform.shape)
     print(sample_rate)
     print(utterance)
     print(speaker_id)
+    print(chapter_id)
     print(utterance_id)
 
+    libriGen = LibriGenuine("/dataNVME/neil/libriSpeech/", feature='LFCC', feat_len=750, pad_chop=True, padding='repeat')
+    print(len(libriGen))
+    featTensor, tag, label = libriGen[123]
+    print(featTensor.shape)
+    print(tag)
+    print(label)
--- preprocess.py
+++ preprocess.py
@@ -3,14 +3,30 @@ from feature_extraction import LFCC
 import os
 import torch
 
-vctk = dataset.VCTK_092(root="/data/neil/VCTK", download=False)
-target_dir = "/dataNVME/neil/VCTK/LFCC"
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+cuda = torch.cuda.is_available()
+print('Cuda device available: ', cuda)
+device = torch.device("cuda" if cuda else "cpu")
+
+# vctk = dataset.VCTK_092(root="/data/neil/VCTK", download=False)
+# target_dir = "/dataNVME/neil/VCTK/LFCC"
+# lfcc = LFCC(320, 160, 512, 16000, 20, with_energy=False)
+# for idx in range(len(vctk)):
+#     print("Processing", idx)
+#     waveform, sample_rate, utterance, speaker_id, utterance_id = vctk[idx]
+#     lfccOfWav = lfcc(waveform)
+#     torch.save(lfccOfWav, os.path.join(target_dir, "%d_%s_%s.pt" %(idx, speaker_id, utterance_id)))
+# print("Done!")
+
+librispeech = dataset.LIBRISPEECH(root="/data/neil")
+target_dir = "/dataNVME/neil/libriSpeech/LFCC"
 lfcc = LFCC(320, 160, 512, 16000, 20, with_energy=False)
-for idx in range(len(vctk)):
+lfcc = lfcc.to(device)
+for idx in range(len(librispeech)):
     print("Processing", idx)
-    waveform, sample_rate, utterance, speaker_id, utterance_id = vctk[idx]
+    waveform, sample_rate, utterance, speaker_id, chapter_id, utterance_id = librispeech[idx]
+    waveform = waveform.to(device)
     lfccOfWav = lfcc(waveform)
-    torch.save(lfccOfWav, os.path.join(target_dir, "%d_%s_%s.pt" %(idx, speaker_id, utterance_id)))
+    torch.save(lfccOfWav, os.path.join(target_dir, "%d_%s_%s_%s.pt" %(idx, speaker_id, chapter_id, utterance_id)))
 print("Done!")
-
-
